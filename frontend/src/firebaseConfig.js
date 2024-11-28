630625239aec8c96553a9401ae3c831cd6f71402
RawSalmon <phanthawasjira@Gmail.com>
2024-11-28 16:38:06 +0700

    refactor frontend config system

---
+++ frontend/src/firebaseConfig.js
@@ -0,0 +1,23 @@
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { config } from 'dotenv';
+
+config(); // Load environment variables from .env file
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app); // Get the database instance
+
+export { database };
