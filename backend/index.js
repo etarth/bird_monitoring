7ec8da9168c74dfa9f64de54c83917b6513c289d
RawSalmon <phanthawasjira@Gmail.com>
2024-11-28 16:32:51 +0700

    refactor backend config system

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const path = require('path');
+require('dotenv').config(); // Load environment variables from .env file
 
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://bird-monitoring-system-default-rtdb.asia-southeast1.firebasedatabase.app'
+  databaseURL: process.env.DATABASE_URL
 });
 
 const db = admin.database(); // Access the Firebase Realtime Database
 
 // Initialize Express app
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON requests
 app.use(express.json());
